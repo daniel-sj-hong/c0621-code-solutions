af19d16353b066f4c532a973c588d1f35df07fc6
Daniel Hong <dan8492@gmail.com>
2021-06-29 11:11:22 -0700

    commiting changes

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,1 +1,9 @@
 /* exported isVowel */
+function isVowel(character) {
+  var container = character.toLowerCase();
+  if (container === 'a' || container === 'e' || container === 'i' || container === 'o' || container === 'u') {
+    return true;
+  } else {
+    return false;
+  }
+}
