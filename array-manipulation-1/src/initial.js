6716d2e12531d3992aa13346d948063b5012d6d9
Daniel Hong <dan8492@gmail.com>
2021-07-02 14:53:49 -0700

    made suggested change by starting loop at 0 and making the loop stop before it gets to the end

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -1,8 +1,8 @@
 /* exported initial */
 function initial(array) {
   var container = [];
-  for (var i = 1; i < array.length; i++) {
-    container.push(array[i - 1]);
+  for (var i = 0; i < array.length - 1; i++) {
+    container.push(array[i]);
   }
   return container;
 }
