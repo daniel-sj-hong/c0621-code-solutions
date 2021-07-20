9048d6fe80c93d761bcb73721e7e37b842760715
Daniel Hong <dan8492@gmail.com>
2021-07-20 16:33:45 -0700

    in progress

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,4 +1,13 @@
 /* exported chunk */
 function chunk(array, size) {
-
+  var finalArray = [];
+  var weirdArray = [];
+  for (var i = 0; i < array.length; i++) {
+    weirdArray.push(array[i]);
+    if (weirdArray.length === size) {
+      finalArray.push(weirdArray);
+      weirdArray = [];
+    }
+  }
+  return finalArray;
 }
