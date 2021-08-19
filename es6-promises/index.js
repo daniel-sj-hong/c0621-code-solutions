1de95f93236eb2eaf602f2f50f66258a33e18c52
Daniel Hong <dan8492@gmail.com>
2021-08-18 17:34:43 -0700

    completed es6-promises

---
+++ es6-promises/index.js
@@ -0,0 +1,11 @@
+const takeAChance = require('./take-a-chance');
+
+const variable = takeAChance('daniel');
+
+variable.then(value => {
+  console.log(value);
+});
+
+variable.catch(error => {
+  console.error(error);
+});
