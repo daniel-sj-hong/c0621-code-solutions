5bac60f5afb3b93ebd0546335d9e113e80246f46
Daniel Hong <dan8492@gmail.com>
2021-08-23 16:53:38 -0700

    completed react-elements

---
+++ react-elements/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const react = React.createElement(
+  'h1',
+  null,
+  'Hello, React!'
+);
+
+ReactDOM.render(react, document.getElementById('root'));
