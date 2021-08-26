a3ec5504a21579d39225b354b8d2247cc12dbb27
Daniel Hong <dan8492@gmail.com>
2021-08-26 13:13:30 -0700

    completed react-form-controls

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange() {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit() {
+    console.log('state: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit} id="form">
+          Email<input onChange={this.handleChange} type="text"></input>
+          <button>Sign Up!</button>
+        </form>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NewsletterForm />,
+  document.querySelector('#root')
+);
