efd49911766538cc7cf9a9fb5d78b33ce99438b5
Daniel Hong <dan8492@gmail.com>
2021-10-08 23:31:54 -0700

    previous method used big O notation and a for loop i'm not used to so i used a new method

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,11 +1,10 @@
 /* exported pick */
 function pick(source, keys) {
   var object = {};
-  var sourceKeys = Object.keys(source);
-  sourceKeys.forEach(function (sourceKey) {
-    if (keys.includes(sourceKey) && source[sourceKey] !== undefined) {
-      object[sourceKey] = source[sourceKey];
+  for (var i = 0; i < keys.length; i++) {
+    if (source[keys[i]] !== undefined) {
+      object[keys[i]] = source[keys[i]];
     }
-  });
+  }
   return object;
 }
