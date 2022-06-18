3f697ff5a41f8b3dbc59d377365d56bb76da2c7d
Daniel Hong <dan8492@gmail.com>
2022-06-17 23:29:46 -0700

    created a separate solution for reverse word. practice

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -13,3 +13,26 @@ function reverseWords(string) {
   reversedString += reversedWord;
   return reversedString;
 }
+
+// function reverseWords(string) {
+//   const split = string.split(' ');
+//   console.log(split);
+//   const arr = [];
+
+//   for (let i = 0; i < split.length; i++) {
+//     const word = split[i];
+//     let reversedString = ''; // llA
+//     console.log(reversedString);
+//     for (let j = word.length - 1; j >= 0; j--) { // j = 0
+//       const letter = word[j];
+//       reversedString += split[i];
+//     }
+//     arr.push(reversedString);
+//   }
+//   return arr.join(' ');
+// }
+
+//   loop through the split array and get each word
+//     loop through the word and start at the end of the word and decrement
+//     add each letter to reversedString
+//   push reversedString to arr
